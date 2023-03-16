9fce42d97e78c49795d9f53e05e7ba957c54dc9d
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-03-16 23:06:22 +0100

    task 1 update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,6 +7,8 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+btnStop.disabled = true;
+
 btnStart.addEventListener('click', () => {
   const colorSwitch = () => {
     body.style.backgroundColor = getRandomHexColor();
