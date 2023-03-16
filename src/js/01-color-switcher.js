3d29b9023999e8b156f243eff951b901ad68898f
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-03-16 07:30:07 +0100

    taks 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
+const btnStart = document.querySelector('data-start');
+const btnStop = document.querySelector('data-stop');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startClick = () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+};
+
+btnStart.addEventListener('click', startClick);
