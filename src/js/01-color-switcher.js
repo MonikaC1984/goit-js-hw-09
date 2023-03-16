0d90c0562a493d001aa6f4cfa883fb0d5e7ccc23
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-03-16 08:57:03 +0100

    bundler

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,5 @@
-const btnStart = document.querySelector('data-start');
-const btnStop = document.querySelector('data-stop');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
 
 function getRandomHexColor() {
