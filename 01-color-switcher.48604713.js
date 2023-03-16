ca806e1d2ff9ab4b666b376939eb8256f20ed2e7
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-03-16 06:30:28 +0000

    Deploying to gh-pages from @ MonikaC1984/goit-js-hw-09@3d29b9023999e8b156f243eff951b901ad68898f 🚀

---
+++ 01-color-switcher.48604713.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("data-start"),o=(document.querySelector("data-stop"),document.querySelector("body"));t.addEventListener("click",(()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=t}));
+//# sourceMappingURL=01-color-switcher.48604713.js.map
