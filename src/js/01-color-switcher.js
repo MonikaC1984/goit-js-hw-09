44a5b140f5557fef86d20f5f2e7de701db0d60f9
MonikaC1984 <113378661+MonikaC1984@users.noreply.github.com>
2023-03-16 09:51:25 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const btnStart = document.querySelector('[data-start]');
 const btnStop = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
+let timerId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -10,11 +11,12 @@ function colorSwitch() {
   body.style.backgroundColor = getRandomHexColor();
 }
 
-function startClick() {
-  setTimeout(() => {
-    colorSwitch;
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    console.log('to jest zmiana koloru:', colorSwitch);
   }, 1000);
-}
+});
 
-btnStart.addEventListener('click', startClick);
-//btnStop.addEventListener('click', stopClick);
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+});
